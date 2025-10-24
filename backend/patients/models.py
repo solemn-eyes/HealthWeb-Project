e3c9e21e0871f312ba41bdfc6e8c32c7db29dc8f
Jerome Maunga <jerhomemaunga@gmail.com>
2025-10-24 11:45:48 +0300

    Modified and made the patient's models

--- backend/patients/models.py
+++ backend/patients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Patient's models
+
+class Patient(AbstractUser):
+    phone = models.CharField(max_length=15, blank=True)
+    gender = models.CharField(max_length=10, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
