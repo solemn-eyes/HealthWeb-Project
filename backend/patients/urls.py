efa8e3bb4d6f55b60f75167d00cdfedf776ad877
Jerome Maunga <jerhomemaunga@gmail.com>
2025-11-14 21:47:38 +0300

    Added the PatientsDetails views

--- backend/patients/urls.py
+++ backend/patients/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshSlidingView
-from .views import register_view
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import register_view, patient_profile_view
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshSlidingView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', register_view, name='register'),
+    path('me/', patient_profile_view, name='patient_profile'),
 ]
