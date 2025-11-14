8ec16c5ce7f91ad03bc5f9e8fb1c4059cd3b822e
Jerome Maunga <jerhomemaunga@gmail.com>
2025-11-14 13:07:13 +0300

    Fixed typos

--- backend/patients/urls.py
+++ backend/patients/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshSlidingView
+from .views import register_view
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshSlidingView.as_view(), name='token_refresh'),
+    path('register/', register_view, name='register'),
 ]
