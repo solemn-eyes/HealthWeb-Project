e78daa5dc684358d1d002c9ab34236e0083cbed2
Jerome Maunga <jerhomemaunga@gmail.com>
2025-11-14 13:07:35 +0300

    Fixed typos

--- backend/patients/views.py
+++ backend/patients/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+@api_view(['POST'])
+def register_view(request):
+	serializer = RegisterSerializer(data=request.data)
+	if serializer.is_valid():
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
+	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
