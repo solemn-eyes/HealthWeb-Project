d0f64a307e41319b6b68ee3a758216d63f19a8ca
Jerome Maunga <jerhomemaunga@gmail.com>
2025-11-15 15:19:48 +0300

    Updated the url paths

--- backend/patients/urls.py
+++ backend/patients/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import register_view, patient_profile_view
+from .views import register_view, patient_profile_view, PatientUpdateView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', register_view, name='register'),
     path('me/', patient_profile_view, name='patient_profile'),
+    path('me/update/', PatientUpdateView.as_view(), name='patient_update'),
 ]
