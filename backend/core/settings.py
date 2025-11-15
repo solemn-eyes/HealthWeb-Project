b2308dde2fae87849113b899886dabe5be9d0c3b
Jerome Maunga <jerhomemaunga@gmail.com>
2025-11-15 22:05:03 +0300

    Added a new model feature

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -178,3 +178,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
